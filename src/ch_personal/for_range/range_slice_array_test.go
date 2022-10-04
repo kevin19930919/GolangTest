959101a54e55a4b6b5ba495c6a5ec1bb7444cc25
kevin.tsai <kevin.tsai@kevintsais-MacBook-Pro.local>
2022-10-04 15:46:13 +0800

    feat:add range array and slice

---
+++ src/ch_personal/for_range/range_slice_array_test.go
@@ -0,0 +1,25 @@
+package for_range
+
+import (
+	"testing"
+)
+
+func TestRangeArray(t *testing.T) {
+	a := [5]int{1, 2, 3, 4, 5}
+	for index, value := range a {
+		if index == 0 {
+			a[1] = 10
+		}
+		t.Log("index: ", index, "value: ", value, "value pointer: ", &value)
+	}
+}
+
+func TestRangeSlice(t *testing.T) {
+	a := []int{1, 2, 3, 4, 5}
+	for index, value := range a {
+		if index == 0 {
+            a[1] = 10
+        }
+		t.Log("index: ", index, "value: ", value, "value pointer: ", &value)
+	}
+}
