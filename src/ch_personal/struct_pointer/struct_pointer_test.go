f541c1c24b534f550a575542a6f2957c771daecc
kevin_tsai <kevin19930919@gmail.com>
2022-11-14 08:50:48 +0800

    feat: add test rebase file

--- src/ch_personal/struct_pointer/struct_pointer_test.go
+++ src/ch_personal/struct_pointer/struct_pointer_test.go
@@ -15,3 +15,29 @@ func TestStruct(t *testing.T) {
 	people := NewPeople("kevin", 28)
 	t.Log("name: ", people.name, " age: ", people.age)
 }
+
+// another test
+
+type Grades struct {
+	Math    int
+	Science int
+	Chinese int
+	Total   func(...int) int
+	// Average func(Math, Science, Chinese) int
+}
+
+func TestAnonymousFuncInStruct(t *testing.T) {
+	grades := &Grades{
+		Math:    100,
+		Science: 200,
+		Chinese: 300,
+		Total: func(...int) int {
+			return Math + Science + Chinese
+		},
+	}
+	t.Log(grades.Total)
+}
+
+// func (g *Grades) Total() int {
+// 	return g.Science + g.Chinese + g.Math
+// }
